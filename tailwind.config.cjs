b2f3f224155d4dae6b00f5bd8796c14461b609ff
WilliamTrodd <william.trodd@gmail.com>
2023-06-16 01:11:17 +0300

    Set up index with components

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,21 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontSize: {
+				'hero': [
+					'8rem',
+					{lineHeight: '1',
+					letterSpacing: '-0.01em',
+					fontWeight: '700',
+				}
+				]
+			},
+			colors: {
+				'accent-green': '#91ff00',
+				'green-text': '#8ccb42'
+			}
+		},
 	},
 	plugins: [],
 };
